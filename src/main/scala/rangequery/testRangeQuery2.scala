6b0360dc6459207b1b98774c577831c5d8272c9e
kaiqiboy <35964734+kaiqiboy@users.noreply.github.com>
2020-10-06 13:51:47 +0800

    Update testRangeQuery2.scala

--- src/main/scala/rangequery/testRangeQuery2.scala
+++ src/main/scala/rangequery/testRangeQuery2.scala
@@ -3,6 +3,7 @@ package main.scala.rangequery
 import main.scala.graph.RoadGrid
 import main.scala.mapmatching.preprocessing
 import org.apache.spark.{SparkConf, SparkContext}
+import System.nanoTime
 
 object testRangeQuery2 extends App {
   override def main(args: Array[String]): Unit = {
@@ -16,8 +17,9 @@ object testRangeQuery2 extends App {
     conf.setAppName("RangeQuery_v2").setMaster(master)
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
-
+    var t = nanoTime()
     val queries = preprocessing.readQueryFile(query)
+    val numQueries = queries.length
     val queryRDD = sc.parallelize(queries, numPartition)
     val roadMapRDD = sc.parallelize(
       RoadGrid(roadGraphFile).edges.map(x => (x.ls.mbr, x.id)), numPartition) // (mbr, roadID)
@@ -26,13 +28,17 @@ object testRangeQuery2 extends App {
       .map { case (qRange, (_, id)) => (id, qRange) }
       .groupByKey()
       .map { case (k, v) => (k, v.toArray) } //(roadID, queryBoxes)
-
+    queriedRoadSegs.cache()
+    println(s"--- Generating query RDD takes ${((nanoTime()-t)/10e9).formatted("%.3f")} s")
+    t = nanoTime()
     val subTrajRDD = preprocessing.readMMTrajFile(mmTrajFile)
       .map(x => (x.tripID, x.points.sliding(2).toArray.map(x=>s"${x(0)}-${x(1)}")))
       .flatMapValues(x => x)
       .map { case (x, y) => (y, x) }
       .repartition(numPartition) // (roadID, (speed, tripID))
-
+    subTrajRDD.cache()
+    println(s"--- Generating subtrajectory RDD takes  ${((nanoTime()-t)/10e9).formatted("%.3f")} s")
+    t = nanoTime()
     val res = queriedRoadSegs.join(subTrajRDD).map { case (_, (queries, tripID)) => (tripID, queries) }
       .flatMapValues(x => x)
       .map { case (k, v) => (v, k) }
@@ -43,5 +49,6 @@ object testRangeQuery2 extends App {
       val queryRange = i._1
       println(s"Query Range: $queryRange : ${i._2.length} sub-trajectories")
     }
+    println(s"--- $numQueries range queries takes ${((nanoTime()-t)/10e9).formatted("%.3f")} s")
   }
 }
