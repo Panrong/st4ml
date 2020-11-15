bd227d5e5c94d2b4deb704fff24975f4c6e9b0b9
kaiqi <543100804@qq.com>
2020-11-15 20:35:07 +0800

    Update simpleQueryWithPartitioner.scala

--- src/main/scala/partitioner/simpleQueryWithPartitioner.scala
+++ src/main/scala/partitioner/simpleQueryWithPartitioner.scala
@@ -2,10 +2,12 @@ package partitioner
 
 import java.lang.System.nanoTime
 
-import geometry.{Point, Rectangle}
+import geometry.{Point, Rectangle, Trajectory}
+import mapmatching.preprocessing
 import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.math.{max, min}
+import scala.reflect.ClassTag
 import scala.util.Random
 
 
@@ -13,10 +15,11 @@ object simpleQueryWithPartitioner extends App {
   override def main(args: Array[String]): Unit = {
 
     val master = args(0)
-    val DataNum = args(1).toInt
-    val queryNum = args(2).toInt
+    val trajectoryFile = args(1)
+    val queryFile = args(2)
     val numPartitions = args(3).toInt
     val samplingRate = args(4).toDouble
+    val dataSize = args(5).toInt
 
     /** set up Spark */
     val conf = new SparkConf()
@@ -24,25 +27,30 @@ object simpleQueryWithPartitioner extends App {
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
 
-    /** generate mock points */
-    var data = new Array[Point](0)
-    val r = new Random(10)
-    for (_ <- 0 until DataNum) data = data :+
-      Point(r.nextDouble * 100, r.nextDouble * 100)
-    val rdd = sc.parallelize(data, numPartitions)
+    //    /** generate mock points */
+    //    var data = new Array[Point](0)
+    //    val r = new Random(10)
+    //    for (_ <- 0 until DataNum) data = data :+
+    //      Point(r.nextDouble * 100, r.nextDouble * 100)
+    //    val rdd = sc.parallelize(data, numPartitions)
 
-    /** generate mock queries */
-    var queries = new Array[Rectangle](0)
-    for (_ <- 0 until queryNum) {
-      val v1 = r.nextDouble * 100
-      val v2 = r.nextDouble * 100
-      val v3 = r.nextDouble * 100
-      val v4 = r.nextDouble * 100
-      queries = queries :+
-        Rectangle(Point(min(v1, v2), min(v3, v4)), Point(max(v1, v2), max(v3, v4)))
-    }
+    //    /** generate mock queries */
+    //    var queries = new Array[Rectangle](0)
+    //    for (_ <- 0 until queryNum) {
+    //      val v1 = r.nextDouble * 100
+    //      val v2 = r.nextDouble * 100
+    //      val v3 = r.nextDouble * 100
+    //      val v4 = r.nextDouble * 100
+    //      queries = queries :+
+    //        Rectangle(Point(min(v1, v2), min(v3, v4)), Point(max(v1, v2), max(v3, v4)))
+    //    }
+    /** generate trajectory MBR RDD */
+    val rdd = preprocessing.genTrajRDD(trajectoryFile, dataSize).map(_.mbr)
 
+    /** generate query RDD */
+    val queries = preprocessing.readQueryFile(queryFile)
     val queryRDD = sc.parallelize(queries)
+
     var t = nanoTime()
 
     /** normal query */
@@ -58,12 +66,10 @@ object simpleQueryWithPartitioner extends App {
     t = nanoTime()
 
     val (pRDD, gridBound) = gridPartitioner(rdd, numPartitions, samplingRate)
-
-
     val pRDDWithIndex = pRDD.mapPartitionsWithIndex {
       (index, partitionIterator) => {
-        val partitionsMap = scala.collection.mutable.Map[Int, List[Point]]()
-        var partitionList = List[Point]()
+        val partitionsMap = scala.collection.mutable.Map[Int, List[Rectangle]]()//TODO: get class automatically
+        var partitionList = List[Rectangle]() //TODO: get class automatically
         while (partitionIterator.hasNext) {
           partitionList = partitionIterator.next() :: partitionList
         }
