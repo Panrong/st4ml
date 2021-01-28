3593dc6126edaff8843069aa646dd968563f222c
kaiqi <543100804@qq.com>
2021-01-28 10:03:13 +0800

    Update SelectorTest.scala

--- src/main/scala/selection/selector/SelectorTest.scala
+++ src/main/scala/selection/selector/SelectorTest.scala
@@ -53,7 +53,6 @@ object SelectorTest extends App {
     //    val tQuery = (1372700000L, 1372750000L)
     val tQuery = (1399900000L, 1400000000L)
 
-    println(s"\nOriginal trajectory dataset contains ${trajRDD.count} entries")
     println("\n*-*-*-*-*-*-*-*-*-*-*-*")
 
     /** benchmark */
@@ -61,17 +60,17 @@ object SelectorTest extends App {
     val fullSRDD = trajRDD.filter(x => x.intersect(sQuery))
 
     println(s"*- Full scan S: ${fullSRDD.count} -*")
-    val fullSRDDStrict1 = trajRDD.filter(x => x.points.exists(p => p.intersect(sQuery)))
-    //    fullSRDDStrict1.take(5).foreach(x => println((x.tripID, x.points.map(x => (x, x.intersect(sQuery))).deep)))
-    val fullSRDDStrict2 = trajRDD.filter(x => x.strictIntersect(sQuery))
-    println(s"*- Full scan S strict 1 : ${fullSRDDStrict1.count} -*")
-    println(s"*- Full scan S strict 2 : ${fullSRDDStrict2.count} -*")
-    //    fullSRDD.map(_.tripID).saveAsTextFile("queriedTrajs_mbr")
-    //    fullSRDDStrict1.map(_.tripID).saveAsTextFile("queriedTrajs_point")
-    //    fullSRDDStrict2.map(_.tripID).saveAsTextFile("queriedTrajs_linestring")
+    //        val fullSRDDStrict1 = trajRDD.filter(x => x.points.exists(p => p.intersect(sQuery)))
+    //        fullSRDDStrict1.take(5).foreach(x => println((x.tripID, x.points.map(x => (x, x.intersect(sQuery))).deep)))
+    //        val fullSRDDStrict2 = trajRDD.filter(x => x.strictIntersect(sQuery))
+    //        println(s"*- Full scan S strict 1 : ${fullSRDDStrict1.count} -*")
+    //        println(s"*- Full scan S strict 2 : ${fullSRDDStrict2.count} -*")
+    //        fullSRDD.map(_.tripID).saveAsTextFile("queriedTrajs_mbr")
+    //        fullSRDDStrict1.map(_.tripID).saveAsTextFile("queriedTrajs_point")
+    //        fullSRDDStrict2.map(_.tripID).saveAsTextFile("queriedTrajs_linestring")
 
 
-    val fullSTRDD = fullSRDDStrict2.filter(x => {
+    val fullSTRDD = fullSRDD.filter(x => {
       val (ts, te) = x.timeStamp
       (ts <= tQuery._2 && ts >= tQuery._1) || (te <= tQuery._2 && te >= tQuery._1)
     })
@@ -79,37 +78,31 @@ object SelectorTest extends App {
     println("*-*-*-*-*-*-*-*-*-*-*-*")
     println(s"... Full scanning takes ${(nanoTime() - t) * 1e-9} s.\n")
 
-    /**
-     * Usage of spatialSelector (+ indexer + partitioner)
-     */
+        /**
+         * Usage of spatialSelector (+ indexer + partitioner)
+         */
 
-    /** partition */
-    //    println("==== STR ====")
-    //    t = nanoTime()
-    //    val partitioner = new STRPartitioner(numPartitions)
-    //    val pRDD = partitioner.partition(trajRDD)
-    //    val partitionRange = partitioner.partitionRange
-    //    val spatialSelector = new RTreeSelector(sQuery, partitionRange)
-    //    println(s"... Partitioning takes ${(nanoTime() - t) * 1e-9} s.")
-    //
-    //    /** spatial query by filtering */
-    //    t = nanoTime()
-    //    val queriedRDD1 = spatialSelector.query(partitionRange) //.map(x => (x._2.id, x)).groupByKey().flatMap(x => x._2.take(1))
-    //    println(s"==== Queried dataset contains ${queriedRDD1.count} entries (filtering)")
-    //    println(s"... Querying by filtering takes ${(nanoTime() - t) * 1e-9} s.")
-    //
-    //    /** spatial query with index */
-    //    t = nanoTime()
-    //    val queriedRDD2 = spatialSelector.query(pRDD) //.map(x => (x._2.id, x)).groupByKey().flatMap(x => x._2.take(1))
-    //    println(s"==== Queried dataset contains ${queriedRDD2.count} entries (RTree)")
-    //    println(s"... Querying with index takes ${(nanoTime() - t) * 1e-9} s.")
-    //
-    //    /** temporal query by filtering */
-    //    t = nanoTime()
-    //    val temporalSelector = new TemporalSelector(tQuery)
-    //    val queriedRDD3 = temporalSelector.query(queriedRDD2)
-    //    println(s"==== Queried dataset contains ${queriedRDD3.count} entries (ST)")
-    //    println(s"... Temporal querying takes ${(nanoTime() - t) * 1e-9} s.")
+        /** partition */
+        println("==== STR ====")
+        t = nanoTime()
+        val partitioner = new STRPartitioner(numPartitions)
+        val pRDD = partitioner.partition(trajRDD)
+        val partitionRange = partitioner.partitionRange
+        val spatialSelector = new RTreeSelector(sQuery, partitionRange)
+        println(s"... Partitioning takes ${(nanoTime() - t) * 1e-9} s.")
+
+        /** spatial query with index */
+        t = nanoTime()
+        val queriedRDD2 = spatialSelector.query(pRDD) //.map(x => (x._2.id, x)).groupByKey().flatMap(x => x._2.take(1))
+        println(s"==== Queried dataset contains ${queriedRDD2.count} entries (RTree)")
+        println(s"... Querying with index takes ${(nanoTime() - t) * 1e-9} s.")
+
+        /** temporal query by filtering */
+        t = nanoTime()
+        val temporalSelector = new TemporalSelector(tQuery)
+        val queriedRDD3 = temporalSelector.query(queriedRDD2)
+        println(s"==== Queried dataset contains ${queriedRDD3.count} entries (ST)")
+        println(s"... Temporal querying takes ${(nanoTime() - t) * 1e-9} s.")
 
     /** test hash partitioner */
     println("\n==== HASH ====")
@@ -117,15 +110,14 @@ object SelectorTest extends App {
     t = nanoTime()
     val hashPartitioner = new HashPartitioner(numPartitions)
     val pRDDHash = hashPartitioner.partition(trajRDD)
-    pRDDHash.collect
     val partitionRangeHash = hashPartitioner.partitionRange
     val selectorHash = new RTreeSelector(sQuery, partitionRangeHash)
+    pRDDHash.collect
     println(s"... Partitioning takes ${(nanoTime() - t) * 1e-9} s.")
-
     t = nanoTime()
     val queriedRDD2Hash = selectorHash.query(pRDDHash)
     println(s"==== Queried dataset contains ${queriedRDD2Hash.count} entries (RTree)")
-    println(s"... Querying with index takes ${(nanoTime() - t) * 1e-9} s.")
+    println(s"... Querying with optimization takes ${(nanoTime() - t) * 1e-9} s.")
 
     t = nanoTime()
     val temporalSelectorH = new TemporalSelector(tQuery)
