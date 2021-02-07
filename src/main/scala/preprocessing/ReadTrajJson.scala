1f70e52e01004656b6a7af3e1efee341d9264025
Kaiqi <543100804@qq.com>
2021-02-07 15:02:41 +0800

    add new file

--- src/main/scala/preprocessing/ReadTrajJson.scala
+++ src/main/scala/preprocessing/ReadTrajJson.scala
@@ -1,5 +1,11 @@
 package preprocessing
 
-object readTrajJson {
+import org.apache.spark.sql.SparkSession
 
+object ReadTrajJson {
+def apply(fileName: String):Unit = {
+  val spark = SparkSession.builder().getOrCreate()
+  val df = spark.read.json(fileName)
+  df.show
+}
 }
--- src/test/scala/fileReading/FileReadingSuite.scala
+++ src/test/scala/fileReading/FileReadingSuite.scala
@@ -4,7 +4,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
 import org.scalatest.BeforeAndAfter
 import org.scalatest.funsuite.AnyFunSuite
-import preprocessing.{ReadPointFile, ReadTrajFile, ReadTrajJsonFile}
+import preprocessing._
 
 import java.lang.System.nanoTime
 import scala.io.Source
@@ -60,6 +60,13 @@ class FileReadingSuite extends AnyFunSuite with BeforeAndAfter {
     sc.setLogLevel("ERROR")
     val trajRDD = ReadTrajFile("preprocessing/traj_short.csv", 1000)
     trajRDD.take(5).foreach(println(_))
+  }
+
+  test("test reading trajectory json") {
+    val spark = SparkSession.builder().master("local").getOrCreate()
+    val sc = spark.sparkContext
+    sc.setLogLevel("ERROR")
+    val trajRDD = ReadTrajJson("datasets/traj_template.json")
   }
 
   def afterEach() {
