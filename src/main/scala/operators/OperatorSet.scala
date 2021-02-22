a86858a9479f87d56f28b16163e1caea123294e6
kaiqi <543100804@qq.com>
2021-02-22 19:07:40 +0800

    Update OperatorSet.scala

--- src/main/scala/operators/OperatorSet.scala
+++ src/main/scala/operators/OperatorSet.scala
@@ -2,7 +2,7 @@ package operators
 
 abstract class OperatorSet {
   /** an OperatorSet has to have the following three operators */
-  val selector: _
-  val converter: _
-  val extractor: _
+  val selector: Any
+  val converter: Any
+  val extractor: Any
 }
