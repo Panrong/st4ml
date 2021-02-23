33bcbf76df4028f41099e17c97aa1d37ef983e13
kaiqi <543100804@qq.com>
2021-02-23 19:50:21 +0800

    Update CompanionTest.scala

--- src/main/scala/examples/CompanionTest.scala
+++ src/main/scala/examples/CompanionTest.scala
@@ -1,12 +1,13 @@
 package examples
 
+import geometry.{Rectangle, Trajectory}
+import operators.convertion.Converter
 import operators.extraction.PointCompanionExtractor
-import geometry.{Point, Rectangle}
+import operators.selection.partitioner.HashPartitioner
+import operators.selection.selectionHandler.{RTreeHandler, TemporalSelector}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import preprocessing.ReadTrajFile
-import operators.selection.partitioner.HashPartitioner
-import operators.selection.selectionHandler.{RTreeHandler, TemporalSelector}
 
 import java.lang.System.nanoTime
 import scala.io.Source
@@ -38,39 +39,39 @@ object CompanionTest extends App {
   val dataSize = args(2).toInt
 
 
-  val trajRDD: RDD[geometry.Trajectory] = ReadTrajFile(trajectoryFile, num = dataSize).repartition(numPartitions)
+  val trajRDD: RDD[geometry.Trajectory] = ReadTrajFile(trajectoryFile, num = dataSize, limit = true).repartition(numPartitions)
 
   val sQuery = Rectangle(Array(-8.682329739182336, 41.16930767535641, -8.553892156181982, 41.17336956864337))
   val tQuery = (0L, 1500000000L)
 
-  val pointRDD = trajRDD.flatMap(traj => {
-    traj.points.zipWithIndex.map(x => x._1.setID(traj.hashCode().abs + x._2.toString))
-  })
-
   /** initialise operators */
 
   val partitioner = new HashPartitioner(numPartitions)
-  val pRDD = partitioner.partition(pointRDD)
+  val pRDD = partitioner.partition(trajRDD)
   val partitionRange = partitioner.partitionRange
   val spatialSelector = RTreeHandler(partitionRange)
   val temporalSelector = new TemporalSelector
 
   /** step 1: selection */
 
-  val sRDD: RDD[(Int, Point)] = spatialSelector.query(pRDD)(sQuery)
+  val sRDD: RDD[(Int, Trajectory)] = spatialSelector.query(pRDD)(sQuery)
   val stRDD = temporalSelector.query(sRDD)(tQuery)
 
-  println(s"${sRDD.count} points after spatial filtering")
-  println(s"${stRDD.count} points after spatio-temporal filtering")
+  println(s"${stRDD.count} trajectories after spatio-temporal filtering")
+
+  /** step 2: Conversion */
+  val converter = new Converter
+  val pointRDD = converter.traj2Point(stRDD)
+  println(s"${pointRDD.count} points after conversion")
 
   /** step 3: extraction */
   t = nanoTime()
   val extractor = new PointCompanionExtractor
-  val extractedO = extractor.optimizedExtract(500, 100)(stRDD.map(x => x._2))
+  val extractedO = extractor.optimizedExtract(500, 100)(pointRDD)
   println(s"${extractedO.length} pairs have companion relationship (optimized)")
   println(s"... Optimized scanning takes ${(nanoTime() - t) * 1e-9} s.")
   //    println(extracted2.sorted.deep)
-  val extracted = extractor.extract(500, 100)(stRDD.map(x => x._2))
+  val extracted = extractor.extract(500, 100)(pointRDD)
   println(s"${extracted.length} pairs have companion relationship (full scan)")
   println(s"... Full scanning takes ${(nanoTime() - t) * 1e-9} s.")
   //    println(extracted.sorted.deep)
