87250f78289d4591fa132ba454ac45e0ee17f3aa
kaiqi <543100804@qq.com>
2021-03-23 14:10:58 +0800

    added companionDaily

---
+++ src/main/scala/examples/CompanionDailyDev.scala
@@ -0,0 +1,63 @@
+package examples
+
+import geometry.Rectangle
+import operators.CustomOperatorSet
+import operators.convertion.Converter
+import operators.extraction.PointCompanionExtractor
+import operators.selection.DefaultSelector
+import org.apache.spark.sql.SparkSession
+import preprocessing.ReadTrajJson
+import utils.Config
+import utils.TimeParsing.parseTemporalRange
+
+object CompanionDailyDev {
+  def main(args: Array[String]): Unit = {
+
+    /** set up Spark environment */
+    val spark = SparkSession
+      .builder()
+      .appName("CompanionDaily")
+      .master(Config.get("master"))
+      .getOrCreate()
+    val sc = spark.sparkContext
+    sc.setLogLevel("ERROR")
+
+    /** parse input arguments */
+    val trajectoryFile = Config.get("hzData")
+    val numPartitions = Config.get("numPartitions").toInt
+    val sQuery = Rectangle(args(0).split(",").map(_.toDouble))
+    val tQuery = parseTemporalRange(args(1)) //should be one day with margin (e.g. 1 hour)
+    val queryThreshold = args(2).split(",").map(_.toDouble)
+    val sThreshold = queryThreshold.head
+    val tThreshold = queryThreshold.last
+    /**
+     * example input arguments: "118.35,29.183,120.5,30.55" "2020-07-31 23:30:00,2020-08-02 00:30:00" "1000,600"
+     */
+
+    /** initialize operators */
+    val operator = new CustomOperatorSet(
+      DefaultSelector(numPartitions),
+      new Converter,
+      new PointCompanionExtractor)
+
+    /** read input data */
+    val trajRDD = ReadTrajJson(trajectoryFile, numPartitions)
+
+    /** step 1: Selection */
+    val rdd1 = operator.selector.query(trajRDD, sQuery, tQuery)
+    //    rdd1.cache()
+
+    /** step 2: Conversion */
+    val rdd2 = operator.converter.traj2Point(rdd1)
+    //    rdd2.cache()
+    println(s"--- Total points: ${rdd2.count}")
+
+    /** step 3: Extraction */
+    val companionPairs = operator.extractor.optimizedExtract(sThreshold, tThreshold)(rdd2)
+    println("=== Companion Analysis done: ")
+    println("=== 5 examples: ")
+    companionPairs.take(5).foreach { case (q, c) => println(s"  ... $q: ${c.deep}") }
+    sc.stop()
+  }
+
+}
