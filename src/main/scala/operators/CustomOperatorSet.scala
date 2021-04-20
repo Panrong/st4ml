218622ce34481f3fab99cfaf53a34d620fa1a169
kaiqi <543100804@qq.com>
2021-04-20 13:36:12 +0800

    Update CustomOperatorSet.scala

--- src/main/scala/operators/CustomOperatorSet.scala
+++ src/main/scala/operators/CustomOperatorSet.scala
@@ -1,5 +1,8 @@
 package operators
 
+import operators.convertion.Converter
+import operators.selection.DefaultSelector
+
 class CustomOperatorSet[S, C, E]
-  (override val selector: S, override val converter: C, override val extractor: E)
+  (override val selector: S, override val converter: C = new Converter, override val extractor: E)
   extends OperatorSet
