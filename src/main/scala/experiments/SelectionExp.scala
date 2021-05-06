4f2cb35f372c83aa91b61b2012a3da30fdeec713
kaiqi <543100804@qq.com>
2021-05-06 15:06:41 +0800

    Update SelectionExp.scala

--- src/main/scala/experiments/SelectionExp.scala
+++ src/main/scala/experiments/SelectionExp.scala
@@ -1,14 +1,19 @@
 package experiments
 
-import geometry.Rectangle
+import geometry.{Point, Rectangle, Shape}
 import operators.convertion.Traj2PointConverter
 import operators.selection.DefaultSelector
+import operators.selection.indexer.RTree
+import operators.selection.partitioner.HashPartitioner
+import operators.selection.selectionHandler.{RTreeHandler, TemporalSelector}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.storage.StorageLevel
 import preprocessing.ReadTrajJson
 import utils.Config
 
 import java.lang.System.nanoTime
+import scala.math.max
 import scala.util.Random
 
 /**
@@ -38,22 +43,51 @@ object SelectionExp extends App {
   val spatialRange = Rectangle(Array(118.35, 29.183, 120.5, 30.55))
   val temporalRange = (pointRDD.map(_.timeStamp._1).min, pointRDD.map(_.timeStamp._1).max)
 
+  //  for (selectivity <- List(0.5, 0.1, 0.01)) {
+  //    println(s"=== Selectivity: $selectivity")
+  //    val tQuery = genTQuery(temporalRange, selectivity)
+  //    val sQuery = genSQuery(spatialRange, selectivity)
+  //
+  //    var t = nanoTime()
+  //    val selector = DefaultSelector(numPartitions)
+  //    val selected = selector.query(pointRDD, sQuery, tQuery)
+  //    println(s"--- ${selected.count} points selected ")
+  //    println(s"... ST-Tool takes ${((nanoTime() - t) * 1e-9).formatted("%.3f")} s.")
+  //
+  //    t = nanoTime()
+  //    val benchmark = pointRDD.filter(_.inside(sQuery)).filter(x => temporalOverlap(x.timeStamp, tQuery))
+  //    println(s"--- ${benchmark.count} points selected ")
+  //    println(s"... Benchmark takes ${((nanoTime() - t) * 1e-9).formatted("%.3f")} s.")
+  //  }
+
+  /** experiments on multiple queries */
+  val partitioner = new HashPartitioner(numPartitions)
+  val partitionRange = partitioner.partitionRange
+  val temporalSelector = new TemporalSelector()
+  val pRDD = partitioner.partition(pointRDD)
+  val spatialSelector = RTreeHandlerMultiple(partitionRange)
+
+  var t = nanoTime()
   for (selectivity <- List(0.5, 0.1, 0.01)) {
-    println(s"=== Selectivity: $selectivity")
+    println(s"\n=== Selectivity: $selectivity")
     val tQuery = genTQuery(temporalRange, selectivity)
     val sQuery = genSQuery(spatialRange, selectivity)
-
-    var t = nanoTime()
-    val selector = DefaultSelector(numPartitions)
-    val selected = selector.query(pointRDD, sQuery, tQuery)
+    val selected = temporalSelector.query(
+      spatialSelector.query(pRDD)(sQuery)
+    )(tQuery)
     println(s"--- ${selected.count} points selected ")
-    println(s"... ST-Tool takes ${((nanoTime() - t) * 1e-9).formatted("%.3f")} s.")
+  }
+  println(s"... ST-Tool takes ${((nanoTime() - t) * 1e-9).formatted("%.3f")} s.")
 
-    t = nanoTime()
+  t = nanoTime()
+  for (selectivity <- List(0.5, 0.1, 0.01)) {
+    println(s"\n=== Selectivity: $selectivity")
+    val tQuery = genTQuery(temporalRange, selectivity)
+    val sQuery = genSQuery(spatialRange, selectivity)
     val benchmark = pointRDD.filter(_.inside(sQuery)).filter(x => temporalOverlap(x.timeStamp, tQuery))
     println(s"--- ${benchmark.count} points selected ")
-    println(s"... Benchmark takes ${((nanoTime() - t) * 1e-9).formatted("%.3f")} s.")
   }
+  println(s"... Benchmark takes ${((nanoTime() - t) * 1e-9).formatted("%.3f")} s.")
   sc.stop()
 
 
@@ -85,5 +119,59 @@ object SelectionExp extends App {
     if (t1._1 >= t2._1 && t1._1 <= t2._2) true
     else if (t2._1 >= t1._1 && t2._1 <= t1._2) true
     else false
+  }
+
+  // not generalized
+  case class RTreeHandlerMultiple(partitionRange: Map[Int, Rectangle],
+                                  var capacity: Option[Int] = None) {
+
+    SparkSession.builder.getOrCreate().sparkContext.getConf.registerKryoClasses(
+      Array(classOf[RTree[_]],
+        classOf[Rectangle],
+        classOf[Shape]))
+
+    val rTreeRDD: Option[RDD[(Int, RTree[Point])]] = None
+
+    def genRTreeRDD(dataRDD: RDD[(Int, Point)]): RDD[(Int, RTree[Point])] = {
+      val c = capacity.getOrElse({
+        val dataSize = dataRDD.count
+        max((dataSize / dataRDD.getNumPartitions / 100).toInt, 100)
+      }) // rule for capacity calculation if not given
+      val dataRDDWithIndex = dataRDD
+        .map(x => x._2)
+        .mapPartitionsWithIndex {
+          (index, partitionIterator) => {
+            val partitionsMap = scala.collection.mutable.Map[Int, List[Point]]()
+            var partitionList = List[Point]()
+            while (partitionIterator.hasNext) {
+              partitionList = partitionIterator.next() :: partitionList
+            }
+            partitionsMap(index) = partitionList
+            partitionsMap.iterator
+          }
+        }
+      dataRDDWithIndex
+        .mapPartitions(x => {
+          val (pIndex, contents) = x.next
+          val entries = contents.map(x => (x, x.id))
+            .zipWithIndex.toArray.map(x => (x._1._1, x._1._2, x._2))
+          val rtree = entries.length match {
+            case 0 => RTree(entries, 0)
+            case _ => RTree(entries, c)
+          }
+          List((pIndex, rtree)).iterator
+        })
+    }
+
+    def query(dataRDD: RDD[(Int, Point)])(queryRange: Rectangle): RDD[(Int, Point)] = {
+      rTreeRDD.getOrElse(genRTreeRDD(dataRDD))
+        .filter {
+          case (_, rtree) => rtree.numEntries != 0 && rtree.root.m_mbr.intersect(queryRange)
+        }
+        .flatMap { case (partitionID, rtree) => rtree.range(queryRange)
+          .filter(x => queryRange.referencePoint(x._1).get.inside(partitionRange(partitionID))) // filter by reference point
+          .map(x => (partitionID, x._1))
+        }
+    }
   }
 }
