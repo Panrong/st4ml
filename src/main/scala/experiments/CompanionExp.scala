704a294bca508c74c7a0ffd110337e5f30e14fc5
kaiqi <543100804@qq.com>
2021-05-27 14:16:27 +0800

    Update CompanionExp.scala

--- src/main/scala/experiments/CompanionExp.scala
+++ src/main/scala/experiments/CompanionExp.scala
@@ -4,12 +4,14 @@ import geometry.{Point, Rectangle, Trajectory}
 import operators.OperatorSet
 import operators.convertion.Traj2PointConverter
 import operators.extraction.PointCompanionExtractor
-import org.apache.spark.sql.SparkSession
+import operators.repartitioner.TSTRRepartitioner
+import operators.selection.DefaultSelector
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.storage.StorageLevel
 import preprocessing.ReadTrajJson
 import utils.Config
-import utils.TimeParsing.parseTemporalRange
-
-import java.lang.System.nanoTime
+import utils.TimeParsing.{nextDay, parseTemporalRange}
 
 object CompanionExp {
   def main(args: Array[String]): Unit = {
@@ -17,7 +19,7 @@ object CompanionExp {
     /** set up Spark environment */
     val spark = SparkSession
       .builder()
-      .appName("CompanionAll")
+      .appName("CompanionDaily")
       .master(Config.get("master"))
       .getOrCreate()
     val sc = spark.sparkContext
@@ -27,23 +29,24 @@ object CompanionExp {
     val trajectoryFile = Config.get("hzData")
     val numPartitions = Config.get("numPartitions").toInt
     val sQuery = Rectangle(args(0).split(",").map(_.toDouble))
-    val tQuery = parseTemporalRange(args(1))
+    val tQuery = parseTemporalRange(args(1)) //should be one day with margin (e.g. 1 hour)
     val queryThreshold = args(2).split(",").map(_.toDouble)
     val sThreshold = queryThreshold.head
     val tThreshold = queryThreshold.last
+    val optimize = args(3).toBoolean
 
     /**
-     * example input arguments: "118.35,29.183,120.5,30.55" "1597132800,1597136400" "1000,600"
+     * example input arguments: "118.35,29.183,120.5,30.55" "2020-07-31 23:30:00,2020-08-02 00:30:00" "1000,600"
      */
 
     /** initialize operators */
-    val operator = new OperatorSet(sQuery, tQuery) {
-      type I = Trajectory
-      type O = Point
-      val converter = new Traj2PointConverter
-      val extractor = new PointCompanionExtractor
+    val operator = new OperatorSet {
+      override type I = Trajectory
+      override type O = Point
+      override val selector = new DefaultSelector[I](sQuery, tQuery)
+      override val converter = new Traj2PointConverter(Some(sQuery), Some(tQuery))
+      override val extractor = new PointCompanionExtractor
     }
-
 
     /** read input data */
     val trajRDD = ReadTrajJson(trajectoryFile, numPartitions)
@@ -54,22 +57,29 @@ object CompanionExp {
 
     /** step 2: Conversion */
     val rdd2 = operator.converter.convert(rdd1)
-    //    rdd2.cache()
-    //    println(rdd2.count)
+    rdd2.persist(StorageLevel.MEMORY_AND_DISK_SER)
+    println(s"--- Total points: ${rdd2.count}")
+    println(s"... Total ids: ${rdd2.map(_.attributes("tripID")).distinct.count}")
+
+
+    //    /** benchmark: full cartesian */
+    //    val companionPairsRDD = rdd2.cartesian(rdd2).filter {
+    //      case (x, y) => operator.extractor.isCompanion(tThreshold, sThreshold)(x, y)
+    //    }.map(x => (x._1.id, Array(x._2.t, x._2.id)))
+    //      .reduceByKey(_ ++ _)
 
     /** step 3: Extraction */
-    var t = nanoTime()
-    val extracted = operator.extractor.optimizedExtract(sThreshold, tThreshold)(rdd2)
-      .mapValues(_.toArray).flatMapValues(x => x)
-    println(s"${extracted.count} pairs have companion relationship (optimized)")
-    println(s"... Optimized scanning takes ${((nanoTime() - t) * 1e-9).formatted("%.3f")} s.")
-
-    t = nanoTime()
-    val benchmark = operator.extractor.extract(sThreshold, tThreshold)(rdd2)
-      .mapValues(_.toArray).flatMapValues(x => x)
-    println(s"${benchmark.count} pairs have companion relationship (optimized)")
-    println(s"... Optimized scanning takes ${((nanoTime() - t) * 1e-9).formatted("%.3f")} s.")
+    val companionPairsRDD = if(optimize) operator.extractor.optimizedExtract(sThreshold, tThreshold)(rdd2)
+    else operator.extractor.extract(sThreshold, tThreshold)(rdd2)
+    companionPairsRDD.persist(StorageLevel.MEMORY_AND_DISK_SER)
+    rdd2.unpersist()
+    println("=== Companion Analysis done: ")
+    //    println(s"Max num companion: ${companionPairsRDD.map(_._2.size).max}")
+    //    println(s"Average num companion: ${companionPairsRDD.map(_._2.size).mean}")
+    println("=== 5 examples: ")
+    companionPairsRDD.take(5).foreach { case (q, c) => println(s"  ... $q: $c") }
 
     sc.stop()
   }
+
 }
