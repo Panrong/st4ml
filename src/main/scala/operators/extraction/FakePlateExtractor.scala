217a5be18a1a10b67fa0414d5a59cf1341d63653
kaiqi <543100804@qq.com>
2021-06-08 21:58:07 +0800

    Update FakePlateExtractor.scala

--- src/main/scala/operators/extraction/FakePlateExtractor.scala
+++ src/main/scala/operators/extraction/FakePlateExtractor.scala
@@ -5,12 +5,14 @@ import org.apache.spark.rdd.RDD
 
 class FakePlateExtractor extends BaseExtractor[Trajectory] {
   def extract(rdd: RDD[Trajectory], speedThreshold: Double): RDD[String] = {
-    rdd.filter(traj => traj.hasFakePlate(speedThreshold)).map(_.id)
+    rdd.filter(x => x.points.length > 1)
+      .filter(traj => traj.hasFakePlate(speedThreshold)).map(_.id)
   }
 
   def extractAndShowDetail(rdd: RDD[Trajectory], speedThreshold: Double):
   RDD[(String, Array[((Long, (Double, Double)), (Long, (Double, Double)), Double)])] = {
-    rdd.map(x => (x.id, x.findAbnormalSpeed(speedThreshold))).filter(_._2.nonEmpty)
+    rdd.filter(x => x.points.length > 1)
+      .map(x => (x.id, x.findAbnormalSpeed(speedThreshold))).filter(_._2.nonEmpty)
   }
 }
 
