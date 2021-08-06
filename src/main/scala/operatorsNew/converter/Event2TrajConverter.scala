bbc076b8817c9a8d72a0f0ae6bf6e5423e7f33d6
kaiqi <543100804@qq.com>
2021-08-06 16:40:46 +0800

    revised conversion

--- src/main/scala/operatorsNew/converter/Event2TrajConverter.scala
+++ src/main/scala/operatorsNew/converter/Event2TrajConverter.scala
@@ -12,15 +12,25 @@ class Event2TrajConverter[EV: ClassTag, ED] extends Converter {
   override def convert(input: RDD[I]): RDD[O] = {
     val event = input.take(1).head
     assert(event.data != None, "The data field cannot be None")
-    input.map(e => (e.data.toString, e))
-      .groupByKey()
-      .filter(_._2.size > 1)
+    //    input.map(e => (e.data.toString, e))
+    //      .groupByKey()
+    //      .filter(_._2.size > 1)
+    //      .mapValues(x => Trajectory[EV, ED](
+    //        pointArr = x.map(_.entries.map(_.spatial)).toArray.flatten,
+    //        durationArr = x.map(_.entries.map(_.duration)).toArray.flatten,
+    //        valueArr = x.map(_.entries.map(_.value)).toArray.flatten,
+    //        x.head.data)
+    //      .sortByTemporal("start"))
+    //      .map(_._2)
+    input.map(e => (e.data.toString, Array(e)))
+      .reduceByKey(_ ++ _)
+      .filter(_._2.length > 1)
       .mapValues(x => Trajectory[EV, ED](
-        pointArr = x.map(_.entries.map(_.spatial)).toArray.flatten,
-        durationArr = x.map(_.entries.map(_.duration)).toArray.flatten,
-        valueArr = x.map(_.entries.map(_.value)).toArray.flatten,
+        pointArr = x.flatMap(_.entries.map(_.spatial)),
+        durationArr = x.flatMap(_.entries.map(_.duration)),
+        valueArr = x.flatMap(_.entries.map(_.value)),
         x.head.data)
-      .sortByTemporal("start"))
-      .map(_._2)
+      .sortByTemporal("start")
+    ).map(_._1)
   }
 }
