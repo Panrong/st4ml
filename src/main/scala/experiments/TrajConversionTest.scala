628f0fecbc3966d49141925c50f3637e116bb4a3
kaiqi <543100804@qq.com>
2021-08-12 12:41:42 +0800

    exp

--- src/main/scala/experiments/TrajConversionTest.scala
+++ src/main/scala/experiments/TrajConversionTest.scala
@@ -1,7 +1,7 @@
 package experiments
 
 import instances.{Duration, Extent, Point, Trajectory}
-import operatorsNew.converter.Traj2SpatialMapConverter
+import operatorsNew.converter.{Traj2EventConverter, Traj2SpatialMapConverter}
 import operatorsNew.selector.DefaultSelector
 import org.apache.spark.sql.SparkSession
 import utils.Config
@@ -48,7 +48,7 @@ object TrajConversionTest {
     if (c == "sm") {
       val f: Array[Trajectory[None.type, String]] => Array[Trajectory[None.type, String]] = x => x
       val xArray = (sQuery.xMin until sQuery.xMax by (sQuery.xMax - sQuery.xMin) / 6).sliding(2).toArray
-      val yArray =  (sQuery.yMin until sQuery.yMax by (sQuery.yMax - sQuery.yMin) / 6).sliding(2).toArray
+      val yArray = (sQuery.yMin until sQuery.yMax by (sQuery.yMax - sQuery.yMin) / 6).sliding(2).toArray
       val sArray = xArray.flatMap(x => yArray.map(y => (x, y))).map(x => Extent(x._1(0), x._2(0), x._1(1), x._2(1)).toPolygon)
       val t = nanoTime
 
@@ -58,13 +58,21 @@ object TrajConversionTest {
       println("traj to spatial map")
       println((nanoTime - t) * 1e-9)
     }
-//    else if (c == "raster") {
-//      val converter2 = new Event2TrajConverter[None.type, String]
-//      val trajRDD = converter2.convert(trajRDD)
-//      println(trajRDD.count)
-//      println("event to trajectory")
-//      println((nanoTime - t) * 1e-9)
-//    }
+    //    else if (c == "raster") {
+    //      val converter2 = new Event2TrajConverter[None.type, String]
+    //      val trajRDD = converter2.convert(trajRDD)
+    //      println(trajRDD.count)
+    //      println("event to trajectory")
+    //      println((nanoTime - t) * 1e-9)
+    //    }
+    else if (c == "event") {
+      val t = nanoTime()
+      val converter = new Traj2EventConverter[None.type, String]
+      val convertedRDD = converter.convert(trajRDD)
+      println(convertedRDD.count)
+      println("traj to spatial map")
+      println((nanoTime - t) * 1e-9)
+    }
     sc.stop()
   }
 }
