dfe1935377987f2a7971a364194a18657e1f1510
kaiqiboy <543100804@qq.com>
2021-08-25 16:45:11 +0800

    rangequery

--- src/main/scala/experiments/TrajRangeQuery.scala
+++ src/main/scala/experiments/TrajRangeQuery.scala
@@ -1,13 +1,26 @@
 package experiments
 
 import experiments.TrajConversionTest.T
-import instances.{Duration, Point, Trajectory}
+import instances.{Duration, Extent, Point, Trajectory}
 import operatorsNew.selector.DefaultSelector
 import org.apache.spark.sql.SparkSession
 import utils.Config
 
+import java.lang.System.nanoTime
+import scala.io.Source
+
 object TrajRangeQuery {
   def main(args: Array[String]): Unit = {
+    val fileName = args(0)
+    val queryFile = args(1)
+    val numPartitions = args(2).toInt
+    // read queries
+    val f = Source.fromFile(queryFile)
+    val queries = f.getLines().toArray.map(line => {
+      val r = line.split(" ")
+      (Extent(r(0).toDouble, r(1).toDouble, r(2).toDouble, r(3).toDouble),
+        Duration(r(4).toLong, r(5).toLong))
+    })
     val spark = SparkSession.builder()
       .appName("flowExp")
       .master(Config.get("master"))
@@ -16,20 +29,23 @@ object TrajRangeQuery {
     val sc = spark.sparkContext
     sc.setLogLevel("ERROR")
 
-    val fileName = args(0)
-    // read parquet
+    val t = nanoTime
+    // read trajectories
     val readDs = spark.read.parquet(fileName)
     import spark.implicits._
     val trajRDD = readDs.as[T].rdd.map(x => {
       val entries = x.entries.map(p => (Point(p.lon, p.lat), Duration(p.t), None))
       Trajectory(entries, x.id)
     })
-    val lonMin = trajRDD.map(_.extent.xMin).filter(x => x > -10 && x < -6).min
-    val latMin = trajRDD.map(_.extent.yMin).filter(x => x > 35 && x < 45).min
-    val lonMax = trajRDD.map(_.extent.xMax).filter(x => x > -10 && x < -6).max
-    val latMax = trajRDD.map(_.extent.yMax).filter(x => x > 35 && x < 45).max
+    trajRDD.cache()
 
-    println(lonMin, latMin, lonMax, latMax)
+    for ((s, t) <- queries) {
+      val selector = new DefaultSelector[Trajectory[None.type, String]](s, t, numPartitions)
+      val sRDD = selector.query(trajRDD)
+      println(sRDD.count)
+    }
+    println((nanoTime - t) * 1e-9)
+
     sc.stop()
   }
 }
