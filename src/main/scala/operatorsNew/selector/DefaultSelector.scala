ece8c11b87c88be85387168f124522f54bb31a6e
kaiqiboy <543100804@qq.com>
2021-08-25 19:07:25 +0800

    new selector

--- src/main/scala/operatorsNew/selector/DefaultSelector.scala
+++ src/main/scala/operatorsNew/selector/DefaultSelector.scala
@@ -11,12 +11,19 @@ class DefaultSelector[R <: Instance[_, _, _] : ClassTag](sQuery: Polygon,
                                                          tQuery: Duration,
                                                          numPartitions: Int) extends Selector[R] {
   val partitioner: HashPartitioner = new HashPartitioner(numPartitions)
+  var partitionedRDD: Option[RDD[R]] = None
+
+  def partition(dataRDD: RDD[R]): RDD[R] = {
+    val pRDD = partitioner.partition(dataRDD)
+    pRDD.cache()
+    partitionedRDD = Some(pRDD)
+    pRDD
+  }
 
   override def query(dataRDD: RDD[R]): RDD[R] = {
-    val repartitionedRDD = partitioner.partition(dataRDD)
-    repartitionedRDD.filter {
-      o => o.intersects(sQuery, tQuery)
-    }
+    val rdd = partitionedRDD.getOrElse(partition(dataRDD))
+    rdd.filter(_.intersects(sQuery, tQuery))
   }
+
 }
 
