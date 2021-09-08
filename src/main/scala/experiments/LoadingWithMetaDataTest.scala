e094f91a8a01981cc170da61fa1bcf0d87ba48e3
kaiqiboy <543100804@qq.com>
2021-09-08 13:31:57 +0800

    metadata test

--- src/main/scala/experiments/LoadingWithMetaDataTest.scala
+++ src/main/scala/experiments/LoadingWithMetaDataTest.scala
@@ -2,6 +2,7 @@ package experiments
 
 import instances.{Duration, Extent, Point, Trajectory}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
 import utils.Config
 
 import java.lang.System.nanoTime
@@ -72,6 +73,35 @@ object LoadingWithMetaDataTest {
 
     println(trajRDD2.count)
     println(s"Loading without metadata ${(nanoTime - t) * 1e-9} s")
+
+    /** test selecting with metadata */
+    t = nanoTime()
+    // recompute to record time
+    val partitionInfo2 = mutable.Map[Int, Extent]()
+    val partitionNum2 = mutable.Map[Int, Int]()
+    val f2 = Source.fromFile(metadata)
+    for (line <- f2.getLines) {
+      val x = line.split(" ").map(_.toDouble)
+      partitionInfo2(x(0).toInt) = Extent(x(1), x(2), x(3), x(4))
+      partitionNum2(x(0).toInt) = x(5).toInt
+    }
+
+    val sRange2 = Extent(range(0), range(1), range(2), range(3))
+    val relatedPartitions2 = partitionInfo2.filter(x => x._2.intersects(sRange2)
+      && partitionNum2(x._1) > 0).keys.toArray
+
+    val readDs3 = spark.read.parquet(fileName).filter(col("pId").isin(relatedPartitions2: _*))
+
+    val trajRDD3 = readDs3.as[T].rdd.map(x => {
+      val entries = x.entries.map(p => (Point(p.lon, p.lat), Duration(p.t), None))
+      Trajectory(entries, x.id)
+    })
+      .repartition(numPartitions)
+      .filter(_.intersects(sRange))
+
+    println(trajRDD3.count)
+    println(s"Loading without metadata ${(nanoTime - t) * 1e-9} s")
+
     sc.stop()
   }
 }
